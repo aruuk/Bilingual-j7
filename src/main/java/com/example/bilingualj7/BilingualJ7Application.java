e98f03bad926eb26cb6f8a276a88dcd7e7e69367
Aruuk <skybanychnek@gmail.com>
2023-03-30 18:12:29 +0600

    Initial commit

---
+++ src/main/java/com/example/bilingualj7/BilingualJ7Application.java
@@ -0,0 +1,13 @@
+package com.example.bilingualj7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualJ7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilingualJ7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilingualj7/BilingualJ7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilingualj7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualJ7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
